90c04fe310a415377a6c8afa6e80d2f7baf7c0cd
yuji91 <yuji91.1227@gmail.com>
2020-05-28 02:02:43 +0900

    説明用の変数を条件式に利用

--- 6_6_complicated_condition.rb
+++ 6_6_complicated_condition.rb
@@ -1,9 +1,11 @@
 class Code < ActiveRecord::Base
   def legacy_code_or_not
-    if (self.test_codes_count == 0 || 
-        self.test_coverage < 50 &&
-        (self.call_back_per_model_avarage > 9 ||
-        self.nested_conditions_level_avarage > 3))
+    is_no_test_code = self.test_codes_count.zero?
+    is_test_coverage_under_half = self.test_coverage < 50
+    is_too_many_model_callback = self.call_back_per_model_avarage > 9
+    is_deep_nested_condition = self.nested_conditions_level_avarage > 3
+
+    if is_no_test_code || is_test_coverage_under_half && (is_too_many_model_callback || is_deep_nested_condition)
       return :legacy_code
     else
       return :not_legacy_code
