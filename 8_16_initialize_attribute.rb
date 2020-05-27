205a4f9de0c22b7e399fdaba5db632b88cccb321
yuji91 <yuji91.1227@gmail.com>
2020-05-28 02:32:32 +0900

    一時変数を問い合わせメソッドに変更

--- 8_16_initialize_attribute.rb
+++ 8_16_initialize_attribute.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
   # このコードのリファクタリングでは、spec/8_16_spec.rbも変更してください。
-  median_score = User.pluck(:score).median 
-  average_score = User.pluck(:score).average 
+  def median_score
+    User.pluck(:score).median
+  end
+
+  def average_score
+    User.pluck(:score).average
+  end
 
   # 中央値と平均値以上はA, 中央値以上はB, それ以外はC
-  def rank(median_score, average_score)
+  def rank
     if self.score >= median_score && self.score >= average_score
       return :A
     elsif self.score >= median_score
@@ -13,10 +18,10 @@ class User < ActiveRecord::Base
       return :C
     end
   end
- 
+
   # 中央値と平均値の半分以下の点数は落第
-  def failed?(median_score, average_score)
-    if self.score <= median_score.div(2) && self.score <= average_score.div(2) 
+  def failed?
+    if self.score <= median_score.div(2) && self.score <= average_score.div(2)
       true
     else
       false
