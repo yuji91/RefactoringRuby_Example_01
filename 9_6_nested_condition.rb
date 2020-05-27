41097059763dfb5365a083c4d51066d47c78b635
yuji91 <yuji91.1227@gmail.com>
2020-05-28 02:21:22 +0900

    各条件の判定部分をメソッドとして抽出

--- 9_6_nested_condition.rb
+++ 9_6_nested_condition.rb
@@ -4,13 +4,13 @@ class Payment < ActiveRecord::Base
 
   def price
     add_tax_to_price
-    if self.coupon_code.present?
+    if has_coupon?
       coupon_price
     else
-      if self.invited_user_id.present?
+      if invited?
         invited_price
       else
-        if self.period >= TIME_SALE_START && self.period <= TIME_SALE_END
+        if in_time_sale?
           time_sale_price
         else
           normal_price
@@ -20,6 +20,18 @@ class Payment < ActiveRecord::Base
   end
 
   private
+
+  def has_coupon?
+    self.coupon_code.present?
+  end
+
+  def invited?
+    self.invited_user_id.present?
+  end
+
+  def in_time_sale?
+    self.period >= TIME_SALE_START && self.period <= TIME_SALE_END
+  end
 
   def add_tax_to_price
     self.base_price += base_price * 0.1
