d500ea4f7bd2c3b39212f081a882ba99ef3ff177
yuji91 <yuji91.1227@gmail.com>
2020-05-28 02:08:06 +0900

    レシーバのselfを省略

--- 6_6_complicated_condition.rb
+++ 6_6_complicated_condition.rb
@@ -1,9 +1,9 @@
 class Code < ActiveRecord::Base
   def legacy_code_or_not
-    def no_test_code?; self.test_codes_count.zero? end
-    def test_coverage_under_half?; self.test_coverage < 50 end
-    def too_many_model_callback?; self.call_back_per_model_avarage > 9 end
-    def deep_nested_condition?; self.nested_conditions_level_avarage > 3 end
+    def no_test_code?; test_codes_count.zero? end
+    def test_coverage_under_half?; test_coverage < 50 end
+    def too_many_model_callback?; call_back_per_model_avarage > 9 end
+    def deep_nested_condition?; nested_conditions_level_avarage > 3 end
 
     if no_test_code? || test_coverage_under_half? && (too_many_model_callback? || deep_nested_condition?)
       return :legacy_code
