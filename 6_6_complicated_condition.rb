6f6f4eecec2438fadcb284c979eb4a0e86598dea
yuji91 <yuji91.1227@gmail.com>
2020-05-28 02:07:24 +0900

    説明用の変数を問い合わせメソッドに変更

--- 6_6_complicated_condition.rb
+++ 6_6_complicated_condition.rb
@@ -1,11 +1,11 @@
 class Code < ActiveRecord::Base
   def legacy_code_or_not
-    is_no_test_code = self.test_codes_count.zero?
-    is_test_coverage_under_half = self.test_coverage < 50
-    is_too_many_model_callback = self.call_back_per_model_avarage > 9
-    is_deep_nested_condition = self.nested_conditions_level_avarage > 3
+    def no_test_code?; self.test_codes_count.zero? end
+    def test_coverage_under_half?; self.test_coverage < 50 end
+    def too_many_model_callback?; self.call_back_per_model_avarage > 9 end
+    def deep_nested_condition?; self.nested_conditions_level_avarage > 3 end
 
-    if is_no_test_code || is_test_coverage_under_half && (is_too_many_model_callback || is_deep_nested_condition)
+    if no_test_code? || test_coverage_under_half? && (too_many_model_callback? || deep_nested_condition?)
       return :legacy_code
     else
       return :not_legacy_code
