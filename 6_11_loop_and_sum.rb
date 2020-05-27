d9b5bbf5a808541b4c5ec9217c5275b8bf6d75c4
yuji91 <yuji91.1227@gmail.com>
2020-05-28 01:52:12 +0900

    sumを使って結果を代入する一時変数を削除

--- 6_11_loop_and_sum.rb
+++ 6_11_loop_and_sum.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
   def self.monthly_sales
-    self.monthly.inject(0) { |result, order| result += (order.sales - order.cost) }
+    self.monthly.sum { |order| order.sales - order.cost }
   end
 
   scope :monthly, -> { where(created_at: Time.now.all_month) }
