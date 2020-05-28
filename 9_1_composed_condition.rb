d5091f5d04f5f82759ce65621bfc4a4cee220dda
konumashuri <re.e.poch89@gmail.com>
2020-05-28 20:58:32 +0900

    :recycle: 9_1_composed_condition

--- 9_1_composed_condition.rb
+++ 9_1_composed_condition.rb
@@ -1,10 +1,6 @@
 class Governor
   def judge(target)
-    if target.air_vent_count.zero? || target.refresh_air_time_per_hour < 2 || target.people_count >= 5 || target.is_each_touchable == true || target.social_distance_meter < 2 
-      :mitsudesu
-    else
-      :notmitsudesu
-    end
+    target.mitsudesu? ? :mitsudesu : :notmitsudesu
   end
 end
 
@@ -12,16 +8,32 @@ require 'teyu'
 
 class Target
   extend Teyu
-  teyu_init air_vent_count: 1,
-   refresh_air_time_per_hour: 2,
-   people_count: 5,
+
+  MAX_PEOPLE = 5
+  MIN_REFRESH_AIR_TIME_PER_HOUR = 2
+  MIN_AIR_VENT_COUNT = 1
+  MIN_SOCIAL_DISTANCE_METER = 2
+
+  teyu_init air_vent_count: MIN_AIR_VENT_COUNT,
+   refresh_air_time_per_hour: MIN_REFRESH_AIR_TIME_PER_HOUR,
+   people_count: MAX_PEOPLE,
    is_each_touchable: false,
-   social_distance_meter: 2
+   social_distance_meter: MIN_SOCIAL_DISTANCE_METER
 
   attr_accessor :air_vent_count,
    :refresh_air_time_per_hour,
    :people_count,
    :is_each_touchable,
    :social_distance_meter
+
+  def mitsudesu?
+    return true if air_vent_count < MIN_AIR_VENT_COUNT
+    return true if refresh_air_time_per_hour < MIN_REFRESH_AIR_TIME_PER_HOUR
+    return true if people_count >= MAX_PEOPLE
+    return true if is_each_touchable
+    return true if social_distance_meter < MIN_SOCIAL_DISTANCE_METER
+
+    false
+  end
 end
 
