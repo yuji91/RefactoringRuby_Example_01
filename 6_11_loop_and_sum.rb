5ab34bc2a0133528e876462de2e69cbe1241cba6
konumashuri <re.e.poch89@gmail.com>
2020-05-28 20:02:31 +0900

    6_11_loop_and_sum

--- 6_11_loop_and_sum.rb
+++ 6_11_loop_and_sum.rb
@@ -1,12 +1,8 @@
 class Order < ActiveRecord::Base
-  def self.monthly_sales
-    result = 0
-    self.monthly.each do |order|
-      result += order.sales - order.cost
-    end
-    return result
-  end 
-
   scope :monthly, -> { where(created_at: Time.now.all_month) }
+
+  def self.monthly_sales
+    @_monthly_sales ||= monthly.map { |order| order.sales - order.cost }.sum
+  end
 end
 
